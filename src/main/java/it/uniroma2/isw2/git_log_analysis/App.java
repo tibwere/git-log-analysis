5224bf85421ff60538b168104b22e6b08a32bec2
Simone <tib.sim.98@gmail.com>
2021-03-12 11:48:27 +0100

    Added comment to class

--- src/main/java/it/uniroma2/isw2/git_log_analysis/App.java
+++ src/main/java/it/uniroma2/isw2/git_log_analysis/App.java
@@ -1,5 +1,10 @@
 package it.uniroma2.isw2.git_log_analysis;
 
+/**
+ * This is the main class of the project
+ * @author Simone Tiberi (M. 0299908)
+ *
+ */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
