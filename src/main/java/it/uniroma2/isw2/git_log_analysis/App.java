ea538dbfee64fba110f4d69e6aeed62d42560606
Simone Tiberi <tib.sim.98@gmail.com>
2021-03-12 11:25:02 +0100

    First version of the project has been loaded

---
+++ src/main/java/it/uniroma2/isw2/git_log_analysis/App.java
@@ -0,0 +1,7 @@
+package it.uniroma2.isw2.git_log_analysis;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/it/uniroma2/isw2/git_log_analysis/AppTest.java
@@ -0,0 +1,13 @@
+package it.uniroma2.isw2.git_log_analysis;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
